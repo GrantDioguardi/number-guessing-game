4edb2822f0a37b2a3e43fe45ed07f33f3126f980
GrantD <grant.a.dioguardi@gmail.com>
2022-07-17 00:31:51 -0400

    Define methods for class UserInput

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class UserInput {
 
+	private Scanner keyboard = new Scanner(System.in);
+	
+	public int inputInteger(String input) {
+		
+		// TODO Take user input and prevent non integer values.
+		
+		return 0;
+	}
+	
+	public String inputString(String input) {
+		
+		// TODO Take user input.
+		
+		return "";
+	}
 }
