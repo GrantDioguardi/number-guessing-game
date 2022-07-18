2b166f858478a78146c75aa70ab3be8964650070
GrantD <grant.a.dioguardi@gmail.com>
2022-07-17 23:41:04 -0400

    Fill out methods of class Player

--- src/Player.java
+++ src/Player.java
@@ -5,35 +5,37 @@ public class Player {
 	private int score;
 	private String name;
 	
-	public Player() {
-		
+	public Player(String nameParam, int livesParam) {
+		lives = livesParam;
+		name = nameParam;
+		score = 0;
 	}
 	
 	public int getLives() {
-		
+		return lives;
 	}
 	
 	public void setLives(int lifeParam) {
-		
+		lives = lifeParam;
 	}
 	
 	public String getName() {
-		
+		return name;
 	}
 	
 	public void setName(String nameParam) {
-		
+		name = nameParam;
 	}
 	
 	public int getScore() {
-		
+		return score;
 	}
 	
 	public void setScore(int scoreParam) {
-		
+		score = scoreParam;
 	}
 	
 	public String toString() {
-		
+		return "Playername: " + name + ", Score: " + score + ", Lives Remaining: " + lives;
 	}
 }
