1b180a092b32a29459072fd3adfde52311b23258
GrantD <grant.a.dioguardi@gmail.com>
2022-07-17 23:28:04 -0400

    Add comments and string input method to UserInput

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.lang.NumberFormatException;
 
 public class UserInput {
 
 	private Scanner keyboard = new Scanner(System.in);
 	
-	public int inputInteger() {
+	public int inputInteger(String prompt) {
 		int integer = 0;
 		
+		// Continue to accept input until users enter an integer.
 		boolean invalidInput = true;
 		do {
 			try {
+				System.out.print(prompt);
 				integer = Integer.valueOf(keyboard.nextLine());
 				invalidInput = false;
-			} catch (InputMismatchException ime) {
-				System.out.println("Invalid input: Only enter integer values.");
+			} catch (NumberFormatException nme) {
+				System.out.println("Invalid input: Only integers accepted.");
 			}
 		} while (invalidInput);
 		
 		return integer;
 	}
 	
-	public String inputString() {
-		
-		// TODO Take user input.
-		
-		return "";
+	public String inputString(String prompt) {
+		System.out.print(prompt);
+		return keyboard.nextLine();
 	}
 }
