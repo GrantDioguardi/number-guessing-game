c26ac3b8fe42f7c101a70b72750987fa2e26fa07
GrantD <grant.a.dioguardi@gmail.com>
2022-07-17 23:33:18 -0400

    Implement method definitions in Player class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,39 @@
 
 public class Player {
 
+	private int lives;
+	private int score;
+	private String name;
+	
+	public Player() {
+		
+	}
+	
+	public int getLives() {
+		
+	}
+	
+	public void setLives(int lifeParam) {
+		
+	}
+	
+	public String getName() {
+		
+	}
+	
+	public void setName(String nameParam) {
+		
+	}
+	
+	public int getScore() {
+		
+	}
+	
+	public void setScore(int scoreParam) {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
